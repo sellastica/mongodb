98cd32f67599f05ed1c87b26abc38a48abf03065
Tomáš Krejčí <tomas@differencemakers.com>
2019-05-31 16:01:40 +0200

    .

--- src/Mapping/DatabaseFactory.php
+++ src/Mapping/DatabaseFactory.php
@@ -7,19 +7,24 @@ class DatabaseFactory implements IDatabaseFactory
 	private $projectFactory;
 	/** @var \Sellastica\MongoDB\Model\ClientFactory */
 	private $clientFactory;
+	/** @var \Sellastica\Core\Model\Environment */
+	private $environment;
 
 
 	/**
 	 * @param \Sellastica\MongoDB\Model\ClientFactory $clientFactory
 	 * @param \Project\Model\ProjectFactory $projectFactory
+	 * @param \Sellastica\Core\Model\Environment $environment
 	 */
 	public function __construct(
 		\Sellastica\MongoDB\Model\ClientFactory $clientFactory,
-		\Project\Model\ProjectFactory $projectFactory
+		\Project\Model\ProjectFactory $projectFactory,
+		\Sellastica\Core\Model\Environment $environment
 	)
 	{
 		$this->projectFactory = $projectFactory;
 		$this->clientFactory = $clientFactory;
+		$this->environment = $environment;
 	}
 
 	/**
@@ -27,7 +32,13 @@ class DatabaseFactory implements IDatabaseFactory
 	 */
 	public function create(): \MongoDB\Database
 	{
-		$database = $this->projectFactory->create()->getDatabase();
+		$project = $this->projectFactory->create();
+		if ($this->environment->isDebugMode()) {
+			$database = $project->getDebugModeDatabase() ?? $project->getDatabase();
+		} else {
+			$database = $project->getDatabase();
+		}
+
 		if (!$database) {
 			throw new \InvalidArgumentException('Unknown database name, it should be defined in the project');
 		}
